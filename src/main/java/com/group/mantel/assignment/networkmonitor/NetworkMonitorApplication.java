0a7157b63172c8fe126be8e79c5fc64d15d66b4a
Susantha <susantha.andradi@gmail.com>
2024-03-23 22:40:17 +1100

    initial empty project added

---
+++ src/main/java/com/group/mantel/assignment/networkmonitor/NetworkMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.group.mantel.assignment.networkmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/mantel/assignment/networkmonitor/NetworkMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.mantel.assignment.networkmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
